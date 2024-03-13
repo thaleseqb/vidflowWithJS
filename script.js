b25837c8fae14bb0f304e409f0739eec07e796bb
thaleseqb <thalesquadros2@gmail.com>
2024-03-13 16:18:20 -0300

    add functions to filter by input and category

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ async function searchAndShowVideos(){
         
         videos.forEach((video) =>{
             containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
                     <img class="img-canal" src="${video.imagem}" alt="logo do canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `
@@ -23,7 +24,45 @@ async function searchAndShowVideos(){
         <p>Houve um erro ao carregar os vídeos: ${error}</p>       
         `
     }
-
 }
 
 searchAndShowVideos();
+
+const searchBar = document.querySelector(".pesquisar__input")
+searchBar.addEventListener('input', filterBytitle);
+
+function filterBytitle() {
+
+    const videos = document.querySelectorAll(".videos__item");
+    const valueBar = searchBar.value.toLowerCase();
+
+    videos.forEach((video) => {
+        const title = video.querySelector(".titulo-video").textContent.toLocaleLowerCase();
+
+        video.style.display = valueBar ? title.includes(valueBar) ? "block" : "none" : "block";
+    });
+}
+
+const categoryFilter = document.querySelectorAll(".superior__item")
+
+categoryFilter.forEach((btn) => {
+    let categoryName = btn.getAttribute("name")
+    
+    btn.addEventListener("click", () => filterByCategory(categoryName))
+})
+
+function filterByCategory(filter) {
+    const videos = document.querySelectorAll(".videos__item")
+
+    videos.forEach((video) => {
+        let cat = video.querySelector(".categoria").textContent.toLocaleLowerCase()
+        let filterCat = filter.toLocaleLowerCase()
+
+        if (!cat.includes(filterCat) && filterCat !== "tudo") {
+            video.style.display = "none"
+        } else {
+            video.style.display = "block"
+        }
+    })
+}
+
